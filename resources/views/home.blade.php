576e0cf1b86d7b8e5acc757ceb7ecb1a105a5f7c
albugowy15 <muhammadbughowy@gmail.com>
2022-08-21 10:28:14 +0700

    display categories

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title>Home page</title>
+    <title>Home - OwiStore</title>
 </head>
 <body>
-    <h1 class="text-2xl font-bold text-blue-500">Hello World</h1>
+    <header class="w-full bg-blue-500 py-3">
+        <nav class="container mx-auto flex items-center justify-between">
+            <h1 class="font-bold text-xl text-white">Owi Store</h1>
+            <ul class="flex justify-between gap-3 text-white">
+                <li>Products</li>
+                <li>Category</li>
+            </ul>
+        </nav>
+    </header>
+    <main class="container py-5 px-6">
+        <h2 class="font-bold text-3xl">Categories</h2>
+        <div class="grid grid-cols-4 gap-3 mt-4">
+            @foreach ($categories as $item)
+            <div class="relative bg-pink-500">
+                <div class="absolute z-10 bg-black bg-opacity-60 w-full h-full">
+                    <div class="h-full flex flex-col items-center justify-center">
+                        <p class="text-white text-center text-lg font-bold">{{$item->category_name}}</p>
+                    </div>
+                </div>
+                <div class="relative object-fill">
+                    <img src={{$item->asset->path}}  class="object-cover"/>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <div class="py-8"/>
+        <h2 class="font-bold text-3xl">Products</h2>
+        <div class="grid grid-cols-4 gap-3 mt-4">
+            @foreach ($products as $item)
+            
+            @endforeach
+        </div>
+
+    </main>
+    <footer></footer>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'categories' => Category::all(),
+        'products' => Product::all()
+    ]);
 });
